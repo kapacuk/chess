14a7d9ef4043269003a9b5aa1d01ab5fc22b6cfc
andreybarsky <andreybarsky@gmail.com>
2016-09-06 15:12:47 +0100

    Add files via upload

---
+++ carl2.py
@@ -0,0 +1,306 @@
+# crappy AI; routinely loses
+# v2 - rewinds instead of simulating boards
+
+from copy import deepcopy
+from chess14 import *
+from numpy import array
+debug = False
+#from random import randint
+
+
+
+# piece square tables- are these symmetrical? I don't think the queen is
+
+pawnmatrix = [
+[ 0,  0,  0,  0,  0,  0,  0,  0],
+[50, 50, 50, 50, 50, 50, 50, 50],
+[10, 10, 20, 30, 30, 20, 10, 10],
+[ 5,  5, 10, 25, 25, 10,  5,  5],
+[ 0,  0,  0, 20, 20,  0,  0,  0],
+[ 5, -5,-10,  0,  0,-10, -5,  5],
+[ 5, 10, 10,-20,-20, 10, 10,  5],
+[ 0,  0,  0,  0,  0,  0,  0,  0]]
+
+knightmatrix = [
+[-50,-40,-30,-30,-30,-30,-40,-50],
+[-40,-20,  0,  0,  0,  0,-20,-40],
+[-30,  0, 10, 15, 15, 10,  0,-30],
+[-30,  5, 15, 20, 20, 15,  5,-30],
+[-30,  0, 15, 20, 20, 15,  0,-30],
+[-30,  5, 10, 15, 15, 10,  5,-30],
+[-40,-20,  0,  5,  5,  0,-20,-40],
+[-50,-40,-30,-30,-30,-30,-40,-50]]
+
+bishopmatrix = [
+[-20,-10,-10,-10,-10,-10,-10,-20],
+[-10,  0,  0,  0,  0,  0,  0,-10],
+[-10,  0,  5, 10, 10,  5,  0,-10],
+[-10,  5,  5, 10, 10,  5,  5,-10],
+[-10,  0, 10, 10, 10, 10,  0,-10],
+[-10, 10, 10, 10, 10, 10, 10,-10],
+[-10,  5,  0,  0,  0,  0,  5,-10],
+[-20,-10,-10,-10,-10,-10,-10,-20]]
+
+rookmatrix = [
+[ 0,  0,  0,  0,  0,  0,  0,  0],
+[ 5, 10, 10, 10, 10, 10, 10,  5],
+[-5,  0,  0,  0,  0,  0,  0, -5],
+[-5,  0,  0,  0,  0,  0,  0, -5],
+[-5,  0,  0,  0,  0,  0,  0, -5],
+[-5,  0,  0,  0,  0,  0,  0, -5],
+[-5,  0,  0,  0,  0,  0,  0, -5],
+[ 0,  0,  0,  5,  5,  0,  0,  0]]
+
+queenmatrix = [
+[-20,-10,-10, -5, -5,-10,-10,-20],
+[-10,  0,  0,  0,  0,  0,  0,-10],
+[-10,  0,  5,  5,  5,  5,  0,-10],
+[ -5,  0,  5,  5,  5,  5,  0, -5],
+[  0,  0,  5,  5,  5,  5,  0, -5],
+[-10,  5,  5,  5,  5,  5,  0,-10],
+[-10,  0,  5,  0,  0,  0,  0,-10],
+[-20,-10,-10, -5, -5,-10,-10,-20]]
+
+kingmatrix_early = [
+[-30,-40,-40,-50,-50,-40,-40,-30],
+[-30,-40,-40,-50,-50,-40,-40,-30],
+[-30,-40,-40,-50,-50,-40,-40,-30],
+[-30,-40,-40,-50,-50,-40,-40,-30],
+[-20,-30,-30,-40,-40,-30,-30,-20],
+[-10,-20,-20,-20,-20,-20,-20,-10],
+[ 20, 20,  0,  0,  0,  0, 20, 20],
+[ 20, 30, 10,  0,  0, 10, 30, 20]]
+
+kingmatrix_late = [
+[-50,-40,-30,-20,-20,-30,-40,-50],
+[-30,-20,-10,  0,  0,-10,-20,-30],
+[-30,-10, 20, 30, 30, 20,-10,-30],
+[-30,-10, 30, 40, 40, 30,-10,-30],
+[-30,-10, 30, 40, 40, 30,-10,-30],
+[-30,-10, 20, 30, 30, 20,-10,-30],
+[-30,-30,  0,  0,  0,  0,-30,-30],
+[-50,-30,-30,-30,-30,-30,-30,-50]]
+
+def reverse(matrix):
+	newmatrix = []
+	for i in range(1,9):
+		newmatrix.append(matrix[-i])
+	return newmatrix
+
+posvalmatrix = {
+'p': pawnmatrix,
+'R': rookmatrix,
+'Q': queenmatrix,
+'N': knightmatrix,
+'B': bishopmatrix,
+'K': kingmatrix_early
+}
+
+piecevals = {
+'p': 100,
+'R': 500,
+'Q': 900,
+'N': 295,
+'B': 305,
+'K': 5000
+}
+
+weights = {'piece_weights': piecevals, 'square_weights': posvalmatrix}
+
+def piece_pos_value(piece):
+	global posvalmatrix
+	piecename = piece.display()[1]
+	piecematrix = posvalmatrix[piecename]
+	if piece.display()[0] == 'w':
+		piecematrix = reverse(piecematrix)
+	if piece.pos is None:
+		return 0
+	else:
+		x = piece.pos[0]-1
+		y = piece.pos[1]-1
+	return piecematrix[y][x]
+
+def positional_advantage(board, col):
+	adv = 0
+	for piece in board.pieces.values():
+		if piece.col == col:
+			colval = 1
+		else:
+			colval = -1
+		adv += (piece_pos_value(piece) * colval)
+		#print "%s positional bonus: %s" % (piece, (piece_pos_value(piece) * colval))
+	return adv
+
+def count_material(board, col):
+	global piecevals
+	val_total = 0
+	for p in board.pieces.values():
+		if p.col == col:
+			val_total += piecevals[p.display()[1]]
+	return val_total
+
+def count_moves(board, col):
+	move_total = 0
+	if col == board.whose_turn:
+		movelist = board.available_moves
+	else:
+		movelist = board.gen_all_moves(col)[0]
+	for p in movelist.values():
+		for m in p:
+			move_total += 1
+	return move_total
+
+def count_threats(board, col): # higher means you have more threats on the enemy
+	threat_total = 0
+	if col != board.whose_turn:
+		movelist = board.threatened_moves
+	else:
+		movelist = board.gen_all_moves(col, attack_map = True)[0]
+	for p in movelist.values():
+		for m in p:
+			threat_total += 1
+	return threat_total
+
+def check_pawns(board, col):
+	pawn_files = []
+	dp = 0 # doubled pawns (two pawns on one file)
+	bp = 0 # blocked pawns (pawns that cannot advance directly forward)
+	ip = 0 # isolated pawns (pawns without a pawn on an adjacent file)
+	for p in board.pieces.values():
+		if isinstance(p, Pawn) and p.col == col and p.pos is not None:
+			if p.pos[0] in pawn_files:
+				dp += 1
+			pawn_files.append(p.pos[0])
+			if p.col == 'white':
+				forward_dir = array((0,1))
+			else:
+				forward_dir = array((0,-1))
+			infront = board.squares[tuple(array(p.pos) + forward_dir)]
+			if isinstance(infront, Piece):
+				bp += 1
+	for f in pawn_files:
+		if f-1 not in pawn_files and f+1 not in pawn_files:
+			ip += 1
+	return dp, bp, ip
+
+
+def evaluate(board, verbose = False):
+	material_weight = 1
+	position_weight = 5
+	#threat_weight = 1
+	bp_weight, ip_weight, dp_weight = 25, 75, 50
+	material_advantage = (count_material(board, 'white') - count_material(board, 'black')) * material_weight
+	pos_advantage = (positional_advantage(board, 'white') - positional_advantage(board, 'black')) * position_weight
+	#mobility = (count_moves(board, 'white') - count_moves(board, 'black')) * mobility_weight
+	#threats = (count_threats(board, 'white') - count_threats(board, 'black')) * threat_weight
+	dp, bp, ip =  tuple(-array(check_pawns(board, 'white')) + array(check_pawns(board, 'black')))
+	problem_pawns = sum([bp*bp_weight, ip*ip_weight, dp*dp_weight])
+
+	if verbose:
+		print "Material advantage: %s" % material_advantage
+		print "Positional advantage: %s" % pos_advantage
+		#print "Threat advantage: %s" % threats
+		print "Doubled, blocked, isolated pawn advantage: %s, %s, %s" % (dp*dp_weight, bp*bp_weight, ip*ip_weight)
+		print "Total favourability for white: %s" % sum([material_advantage, pos_advantage, problem_pawns])
+	return sum([material_advantage, pos_advantage, problem_pawns])
+
+def lookahead(board, depth=2, max_depth=2): # simulates all possible moves for boards at an arbitrary depth
+	new_movelist = {} # here we store the moves at this depth, and the evaluation of their best outcome for us
+	movelist = get_movelist(board)
+	if depth > 0:
+		for move in movelist:
+			#print "%smoving %s from %s to %s" % (' '*depth, board.squares[move[0]], parse(move[0]), parse(move[1]))
+			board.move(move[0], move[1])
+			#print "move history: " + str(board.movehistory)
+			if board.winner == None: # return the value of the best move up the tree
+				newboard_val = lookahead(board, depth-1, depth) # here is the recursion
+			else: # except if the game is over, in which case return a speial value
+				win_vals = {'white': 9999, 'black': -9999, 'nobody': 0}
+				newboard_val = win_vals[board.winner]
+			new_movelist[move] = newboard_val
+			#print "%s-this position: %s" % (' '*depth, newboard_val)
+			#print "%srewinding from %s to %s" % (' '*depth, parse(move[1]), parse(move[0]))
+			board.rewind()
+
+
+		# get best move:
+		vals = [n for n in new_movelist.values()]
+		print "vals: " + str(vals)
+		if board.whose_turn == 'white':
+			m = max(vals)
+		else:
+			m = min(vals)
+		bestmoves = [i for i, j in enumerate(vals) if j == m]
+		bestmove = new_movelist.keys()[bestmoves[0]]
+		print (" "*depth) + "best move after %s at %s to %s is: %s at %s to %s" % (board.squares[board.movehistory[-1][0][1]], parse(board.movehistory[-1][0][0]), parse(board.movehistory[-1][0][1]), board.squares[bestmove[0]], parse(bestmove[0]), parse(bestmove[1]))
+
+		if depth < max_depth:
+			return evaluate(sim_move(board, (bestmove[0], bestmove[1])))
+
+		else:
+			return bestmove
+
+	elif depth == 0:
+		return evaluate(board)
+
+
+
+
+
+
+def get_movelist(board):
+	movelist = []
+	for piece, moves in board.available_moves.iteritems():
+		for movepos in moves:
+			movelist.append((piece.pos, movepos))
+	return movelist
+
+def evaluate_moves(board):
+	move_dict = {}
+	for piece, movelist in board.available_moves.iteritems():
+		for movepos in movelist:
+			simboard = simulate(board)
+			simboard.squares[piece.pos].move(movepos, simboard)
+			move_dict[(piece.pos, movepos)] = evaluate(simboard)
+	return move_dict
+
+def best_move(board):
+	col = board.whose_turn
+	movedict = evaluate_moves(board)
+	vals = [n for n in movedict.values()]
+	if col == 'white':
+		m = max(vals)
+	else:
+		m = min(vals)
+	bestmoves = [i for i, j in enumerate(vals) if j == m]
+	if len(bestmoves) == 1:
+		bestmove = movedict.keys()[bestmoves[0]]
+		return (bestmove[0], bestmove[1])
+	else:
+		bestmove = movedict.keys()[bestmoves[randint(0, len(bestmoves))]]
+		return (bestmove[0], bestmove[1])
+
+def make_best_move(board):
+	#movedict = evaluate_moves(board)
+	bestmove = lookahead(board)
+	print "Moving %s from %s to %s" % (board.squares[bestmove[0]], parse(bestmove[0]), parse(bestmove[1]))
+	board.move(bestmove[0], bestmove[1])
+
+
+def simulate(board):
+	simboard = deepcopy(board)
+	simboard.real_game = False
+	return simboard
+
+def sim_move(board, move): # move should be a tuple of tuples
+	simboard = simulate(board)
+	simboard.move(move[0], move[1])
+	return simboard
+
+
+
+def play_game():
+	mainboard = chess12.Board()
+	mainboard.newgame()
+	while mainboard.winner == None:
+		make_best_move(mainboard)
---
+++ chess_gui.py
@@ -0,0 +1,229 @@
+# -*- coding: utf-8 -*-
+
+# chess intelligence; not done yet
+
+#import chess13
+#import os
+import wx
+import json
+import subprocess
+import threading
+import sys
+
+
+#boardstate = json.loads(chess13.gui_sendboard(mainboard))
+
+def square_to_coord(square):
+	bottom_corner = (480,0)
+	unit = 60
+	squarex = square[0]
+	squarey = square[1]-1
+	newx = (squarex*unit) - 60
+	newy = bottom_corner[0] - (unit + squarey*unit)
+	return(newx, newy)
+
+def coord_to_square(coord):
+	x = coord[0]
+	y = coord[1]
+	squarex = 1 + int(x/60)
+	squarey = 8 - int(y/60)
+	return (squarex, squarey)
+
+def square_to_string(square):
+	x = str(square[0])
+	y = str(square[1])
+	return x+y
+
+def string_to_square(string):
+	return (int(string[0]), int(string[1]))
+
+class ChessBoard(wx.Frame):
+	def __init__(self, parent, title):
+		self.engine = subprocess.Popen('/home/andrey/Dropbox/Python/chess14.py', stdout = subprocess.PIPE, stdin = subprocess.PIPE, bufsize=0, universal_newlines=True)
+		self.engine.stdin.write('{"command": "newGame"}\n')
+		self.engine.stdin.flush()
+		self.activepiece = None
+		self.winner = None
+
+		self.filenames = {'wp': 'Chess_wpawn.png',
+		'bp': 'Chess_bpawn.png',
+		'wK': 'Chess_wking.png',
+		'bK': 'Chess_bking.png',
+		'wQ': 'Chess_wqueen.png',
+		'bQ': 'Chess_bqueen.png',
+		'wB': 'Chess_wbishop.png',
+		'bB': 'Chess_bbishop.png',
+		'wN': 'Chess_wknight.png',
+		'bN': 'Chess_bknight.png',
+		'wR': 'Chess_wrook.png',
+		'bR': 'Chess_brook.png'}
+
+		thread = threading.Thread(target=self.run)
+		thread.setDaemon(True)
+		thread.start()
+
+		wx.Frame.__init__(self, parent, title=title, size=(480,500))
+		self.CreateStatusBar()
+
+		self.chess_panel = wx.Panel(self)
+
+		# menu:
+		filemenu = wx.Menu()
+
+		menuNewGame = filemenu.Append(wx.ID_ANY, "&New Game", "Clear the board and start again")
+		menuAbout = filemenu.Append(wx.ID_ABOUT, "&About", "Information about this program")
+		menuExit = filemenu.Append(wx.ID_EXIT, "E&xit", "Terminate the program")
+
+		menuBar = wx.MenuBar()
+		menuBar.Append(filemenu, "&File")
+		self.SetMenuBar(menuBar)
+
+		self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
+		self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
+		self.Bind(wx.EVT_MENU, self.OnNewGame, menuNewGame)
+
+		self.chess_panel.Bind(wx.EVT_LEFT_DOWN, self.SquareClick)
+		self.chess_panel.Bind(wx.EVT_RIGHT_DOWN, self.SquareRightClick)
+
+		self.Centre()
+
+		self.Show(True)
+
+	def run(self):
+		while True:
+			x = self.engine.stdout.readline()
+			print x
+			wx.CallAfter(self.process_engine, x)
+
+	def process_engine(self, jsonstring):
+		print "received json message from engine: " + jsonstring
+		try:
+			jsondict = json.loads(jsonstring)
+		except Exception as x:
+			print "error: %s" % x
+			sys.exit()
+		message = jsondict[u'message']
+		if message == 'boardState':
+			self.boardstate = jsondict['board']
+			if jsondict['winner'] == 'None':
+				self.whose_turn = jsondict['turn']
+				self.DrawBoard(self.boardstate)
+			else:
+				self.DrawBoard(self.boardstate)
+				self.winner = jsondict['winner']
+				print "The winner is %s" % self.winner.capitalize()
+#				dlg = wx.MessageDialog(self, "%s wins" % self.winner.capitalize())
+#				dlg.ShowModal()
+#				dlg.Destroy()
+		elif message == 'pieceMoves':
+			piece_pos = jsondict['piecePos']
+			movelist = jsondict['moves']
+			squares_to_highlight = [string_to_square(piece_pos)]
+			for square in movelist:
+				squares_to_highlight.append(string_to_square(square))
+
+			self.HighlightSquares(squares_to_highlight)
+			# self.HighlightMoves ?
+		elif message == 'gameOver':
+			self.winner = jsondict['winner']
+			print "%s wins" % self.winner.capitalize()
+
+
+	def HighlightSquares(self, squares):
+		self.DrawBoard(self.boardstate)
+		for square in squares:
+			dc = wx.PaintDC(self.chess_panel)
+			dc.SetPen(wx.Pen('#000000'))
+			r, c = square[0]-1, 8-square[1]
+			dc.SetBrush(wx.Brush('gold'))
+#			if (square[0] + square[1]) % 2 == 0:
+#				dc.SetBrush(wx.Brush('gold'))
+#			else:
+#				dc.SetBrush(wx.Brush('gold'))
+			dc.DrawRectangle(r*60,c*60,60,60)
+
+			piece = self.boardstate[str(square[0])+str(square[1])]
+			self.DrawPiece(square, piece)
+
+
+	def SquareClick(self, e):
+		if self.winner == None:
+			print "Click detected at %s" % e.GetPosition()
+			clickpos = e.GetPosition()
+			clicksquare = coord_to_square(clickpos)
+			active_turn = self.whose_turn[0]
+			piece = self.boardstate[square_to_string(clicksquare)]
+			if piece != 'None' and piece[0] == active_turn:
+				self.activepiece = clicksquare
+				jsonstring =  '{"command": "getMoves", "piecePos": "%s"}\n' % square_to_string(clicksquare)
+				self.engine.stdin.write(jsonstring)
+			elif self.activepiece is not None:
+				jsonstring = '{"command": "sendMove", "piecePos": "%s", "newPos": "%s"}\n' % (square_to_string(self.activepiece), square_to_string(clicksquare))
+				print jsonstring
+				self.activepiece = None
+				self.DrawBoard(self.boardstate)
+				self.engine.stdin.write(jsonstring)
+		else:
+			print "Click detected at %s, but the game is over" % e.GetPosition()
+
+	def SquareRightClick(self, e):
+		print "Click detected at %s" % e.GetPosition()
+		clickpos = e.GetPosition()
+		clicksquare = coord_to_square(clickpos)
+		self.SetStatusText(str(clicksquare))
+		piece = self.boardstate[square_to_string(clicksquare)]
+		if piece != 'None':
+			self.activepiece = clicksquare
+			jsonstring =  '{"command": "getStats", "piecePos": "%s"}\n' % square_to_string(clicksquare)
+			#self.engine.stdin.write(jsonstring)
+		elif self.activepiece is not None:
+			jsonstring = '{"command": "sendMove", "piecePos": "%s", "newPos": "%s"}\n' % (square_to_string(self.activepiece), square_to_string(clicksquare))
+			print jsonstring
+			self.activepiece = None
+			self.DrawBoard(self.boardstate)
+			self.engine.stdin.write(jsonstring)
+
+	def DrawPiece(self, pos, piece):
+		if piece != 'None':
+			coord = square_to_coord(pos)
+			png = wx.Image(self.filenames[piece], wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+			dc = wx.PaintDC(self.chess_panel)
+			dc.DrawBitmap(png, coord[0], coord[1], True)
+
+	def DrawBoard(self, boardstate):
+		dc = wx.PaintDC(self.chess_panel)
+		dc.SetPen(wx.Pen('#000000'))
+		for r in range(0,8):
+			for c in range(0,8):
+				if ((r+1)+(c+1)) % 2 == 0:
+					dc.SetBrush(wx.Brush('tan'))
+				else:
+					dc.SetBrush(wx.Brush('sienna'))
+				dc.DrawRectangle(r*60,c*60,60,60)
+		for x, y in boardstate:
+			piece = boardstate[x+y]
+			if piece != 'None':
+				pos = (int(x), int(y))
+				self.DrawPiece(pos, piece)
+
+	# non game methods:
+
+	def OnAbout(self, e):
+		dlg = wx.MessageDialog(self, "chess ai, routinely loses")
+		dlg.ShowModal()
+		dlg.Destroy()
+
+	def OnExit(self, e):
+		self.Close(True)
+
+	def OnNewGame(self, e):
+		self.engine.stdin.write('{"command": "newGame"}\n')
+
+
+if __name__ == '__main__':
+	app = None
+	app = wx.App(False)
+
+	frame = ChessBoard(None, 'chess_gui')
+	print dir(frame)
+	app.MainLoop()
